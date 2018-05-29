c3ef95f7b90c480ffa422c2bf56e5947fa5ec9f0
rajesh23-drg <rmanjunath@teamdrg.com>
2018-05-29 07:28:46 +0530

    added basic test cases

--- src/app/core/footer/footer.component.spec.ts
+++ src/app/core/footer/footer.component.spec.ts
@@ -1,25 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
+
 
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ FooterComponent ]
     })
+    // if not using webpack then use it
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture.detectChanges();
+    el = fixture.debugElement.query(By.css('footer .footer-content'));
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have text Customer Manager', () => {
+    expect(el.nativeElement.textContent.trim()).toBe('Customer Manager');
   });
 });
--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +19,31 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have created Customer Manager Navbar Title', () => {
+    el = fixture.debugElement.query(By.css('nav .navbar-brand'));
+    expect(el.nativeElement.textContent.trim()).toBe('Customer Manager');
+  });
+
+  it('should have created Customers link', () => {
+    el = fixture.debugElement.query(By.css('nav .collapse .customers'));
+    expect(el.nativeElement.textContent.trim()).toBe('Customers');
+  });
+
+  it('should have created Orders link', () => {
+    el = fixture.debugElement.query(By.css('nav .collapse .orders'));
+    expect(el.nativeElement.textContent.trim()).toBe('Orders');
+  });
+
+  it('should have created About link', () => {
+    el = fixture.debugElement.query(By.css('nav .collapse .about'));
+    expect(el.nativeElement.textContent.trim()).toBe('About');
+  });
+
 });
