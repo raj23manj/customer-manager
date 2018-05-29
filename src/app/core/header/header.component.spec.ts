376406f62c2557daaeb2c87aae2ee8048ae8ad72
rajesh23-drg <rmanjunath@teamdrg.com>
2018-05-29 18:28:45 +0530

    making test cases pass header component

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, async, whenStable, fakeAsync, tick, ComponentFixture } from '@angular/core/testing';
 import {DebugElement} from "@angular/core";
 import {By} from "@angular/platform-browser";
 import { RouterTestingModule } from '@angular/router/testing';
@@ -10,6 +10,7 @@ describe('HeaderComponent When Not Logged In', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
   let el: DebugElement;
+  let authService: AuthService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,6 +24,7 @@ describe('HeaderComponent When Not Logged In', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    authService = TestBed.get(AuthService);
     //fixture.detectChanges();
   });
 
@@ -30,24 +32,62 @@ describe('HeaderComponent When Not Logged In', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have created Customer Manager Navbar Title', () => {
-    el = fixture.debugElement.query(By.css('nav .navbar-brand'));
-    expect(el.nativeElement.textContent.trim()).toBe('Customer Manager');
+  describe('Before Sign In', () => {
+
+    beforeEach(() => {
+      spyOn(authService, 'getStoredToken').and.returnValue({token: ''});
+      component.isAuthenticated();
+      fixture.detectChanges();
+    });
+
+    it('should have created Customer Manager Navbar Title', () => {
+      el = fixture.debugElement.query(By.css('nav .navbar-brand'));
+      expect(el.nativeElement.textContent.trim()).toBe('Customer Manager');
+    });
+
+    it('should have created About link', () => {
+      el = fixture.debugElement.query(By.css('nav .collapse .about'));
+      expect(el.nativeElement.textContent.trim()).toBe('About');
+    });
+
+    it('should have Sign Up! link', () => {
+      el = fixture.debugElement.query(By.css('nav .collapse .auth-links .sign-up'));
+      expect(el.nativeElement.textContent.trim()).toBe('Sign Up!');
+    });
+
+    it('should have Sign In link', () => {
+      el = fixture.debugElement.query(By.css('nav .collapse .auth-links .sign-in'));
+      expect(el.nativeElement.textContent.trim()).toBe('Sign In');
+    });
   });
 
-  xit('should have created Customers link', () => {
-    el = fixture.debugElement.query(By.css('nav .collapse .customers'));
-    expect(el.nativeElement.textContent.trim()).toBe('Customers');
-  });
+  describe('After User Logged In', () => {
+    beforeEach(() => {
+      spyOn(authService, 'getStoredToken').and.returnValue({token: 'jbsaldkjfgsaidjfgilasdjgfis'});
+      component.isAuthenticated();
+      fixture.detectChanges();
+    });
 
-  xit('should have created Orders link', () => {
-    el = fixture.debugElement.query(By.css('nav .collapse .orders'));
-    expect(el.nativeElement.textContent.trim()).toBe('Orders');
-  });
+    it('should have created Customers link', () => {
+      // https://medium.com/@armno/til-mocking-localstorage-and-sessionstorage-in-angular-unit-tests-a765abdc9d87
+      el = fixture.debugElement.query(By.css('nav .collapse .customers'));
+      expect(el.nativeElement.textContent.trim()).toBe('Customers');
+    });
 
-  it('should have created About link', () => {
-    el = fixture.debugElement.query(By.css('nav .collapse .about'));
-    expect(el.nativeElement.textContent.trim()).toBe('About');
+    it('should have created Orders link', () => {
+      el = fixture.debugElement.query(By.css('nav .collapse .orders'));
+      expect(el.nativeElement.textContent.trim()).toBe('Orders');
+    });
+
+    it('should have Sign Out link', () => {
+      el = fixture.debugElement.query(By.css('nav div.float-right .sign-out'));
+      expect(el.nativeElement.textContent.trim()).toBe('Sign Out');
+    });
+
+    it('should have Sign Out link', () => {
+      el = fixture.debugElement.query(By.css('nav .collapse .auth-links .sign-out'));
+      expect(el.nativeElement.textContent.trim()).toBe('Sign Out');
+    });
   });
 
 });
