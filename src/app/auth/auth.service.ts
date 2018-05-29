a89d5e8ed0ff6afd80801954663c6afb8b605197
rajesh23-drg <rmanjunath@teamdrg.com>
2018-05-29 11:51:50 +0530

    signin working fine

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,29 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+  //token: string;
+
   signupUser(email: string, password: string) {
     return firebase.auth().createUserWithEmailAndPassword(email, password);
   }
-}
+
+  signinUser(email: string, password: string){
+    return this.getAuthenticated(email, password)
+               .then(response => {
+                  console.log('Authentication Successful');
+                  return this.getToken();
+                })
+               .catch(error => {
+                  return Promise.reject(error);
+                });
+  }
+
+  private getToken() {
+    return firebase.auth().currentUser.getIdToken();
+  }
+
+  private getAuthenticated(email: string, password: string) {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+
+}// EOF
