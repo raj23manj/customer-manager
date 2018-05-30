af5b1012f712e8ff5c348ede027ca86b2cd025ea
rajesh23-drg <rmanjunath@teamdrg.com>
2018-05-30 12:13:20 +0530

    added test cases for sigin form and passes aot

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import {DebugElement} from "@angular/core";
-import {By} from "@angular/platform-browser";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from './../../auth/auth.service';
 
