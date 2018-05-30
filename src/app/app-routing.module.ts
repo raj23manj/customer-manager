1592a80bc50a27a9a7b5b6e31d95753b68e2f538
rajesh23-drg <rmanjunath@teamdrg.com>
2018-05-30 13:36:58 +0530

    added auth guard service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './core/home/home.component';
 
+// Guard Services
+import { AuthGuard } from './auth/auth-guard.services';
+
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 
 export class AppRoutingModule {
